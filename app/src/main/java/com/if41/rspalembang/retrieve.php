06a2ffeafc4757c0464ee3e393b7a9b0d13c0b60
George0717 <januar.firnando@mhs.mdp.ac.id>
2023-03-10 20:11:59 +0700

    initial commit konek and retieve php

---
+++ app/src/main/java/com/if41/rspalembang/retrieve.php
@@ -0,0 +1,28 @@
+<?php
+
+require('koneksi.php');
+
+$query = "SELECT * FROM tblrumahsakitpalembang";
+
+$execute = mysqli_query($konek, $query);
+
+$cek = mysqli_affected_rows($konek);
+
+if ($cek > 0) {
+    $respone['kode'] = 1;
+    $respone['pesan'] = "Data Tersedia";
+    $respone['Data'] = array();
+
+    while($ambil = mysqli_fetch_object($execute)) {
+        $temp["id"] = $ambil->id;
+        $temp["nama"] = $ambil->nama;
+        $temp["alamat"] = $ambil->alamat;
+        $temp["telepon"] = $ambil->telepon;
+        $temp["foto"] = $ambil->foto;
+
+        array_push($response['data'], $temp);
+     }
+} else {
+    $respone['kode'] = 0;
+    $respone['pesan'] = "Data Tidak Tersedia";
+}
