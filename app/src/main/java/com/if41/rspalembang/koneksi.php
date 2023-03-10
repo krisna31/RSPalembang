de3e91e3ffb75b11f9a9eccb81b69802b86c89ff
krisnaputra31 <*>
2023-03-10 20:11:59 +0700

    initial commit konek and retieve php

---
+++ app/src/main/java/com/if41/rspalembang/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = 'root';
+$db = 'dbrspalembang';
+
+$konek = mysqli_connect($host, $user, $pass, $db) or die("Database MySQL Tidak Terhubung");
